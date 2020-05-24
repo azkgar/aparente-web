f3370ee2da11a59eb009c913e017e850b2757137
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-05-24 18:51:21 -0500

    Add logout function to logout button

--- client/src/components/Admin/MenuTop/MenuTop.js
+++ client/src/components/Admin/MenuTop/MenuTop.js
@@ -2,11 +2,16 @@ import React from "react";
 import {Button} from "antd"
 import{MenuUnfoldOutlined, MenuFoldOutlined, PoweroffOutlined} from "@ant-design/icons"
 import AparenteLogo from "../../../assets/img/png/APARENTE_logo_blanco_sinfondo.png"
+import {logout} from "../../../api/auth";
 
 import "./MenuTop.scss";
 
 export default function MenuTop(props) {
     const {menuCollapsed, setMenuCollapsed} = props;
+    const logoutUser = () => {
+        logout();
+        window.location.reload();
+    }
     return (
         <div className="menu-top">
             <div className="menu-top__left">
@@ -19,7 +24,7 @@ export default function MenuTop(props) {
                 </Button>
             </div>
             <div className="menu-top__right">
-                <Button type="Link">
+                <Button type = "Link" onClick = {logoutUser}>
                 <PoweroffOutlined />
                 </Button>
             </div>
