628c94621a143f903380e7b3c0e68f23a38119c0
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-05-23 21:44:09 -0500

    Creates conection with mongodb at server's index.js file

---
+++ Server/index.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const PORT_SERVER = process.env.PORT || 5000;
+const {API_VERSION, IP_SERVER, PORT_DB} = require("./config");
+
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(
+    `mongodb://${IP_SERVER}:${PORT_DB}/boda`,
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    (err,res) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log("La conexión a la base de datos es correcta.");
+            app.listen(PORT_SERVER, () => {
+                console.log(`http://${IP_SERVER}:${PORT_SERVER}/api/${API_VERSION}/`);
+            });
+        }
+    }
+);
