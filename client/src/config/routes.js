8d94a220ae028cfa6e8af8c200b050de6516df07
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-05-24 19:24:18 -0500

    Create route for users admin page in client folder

--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -5,6 +5,7 @@ import LayoutBasic from "../layouts/LayoutBasic";
 //Admin Pages
 import AdminHome from "../pages/Admin";
 import AdminSignIn from "../pages/Admin/SignIn";
+import AdminUsers from "../pages/Admin/Users";
 
 //User Pages
 import Home from "../pages/Home";
@@ -26,6 +27,11 @@ const routes = [
             {
                 path: "/admin/login",
                 component: AdminSignIn,
+                exact: true
+            },
+            {
+                path: "/admin/users",
+                component: AdminUsers,
                 exact: true
             },
             {
