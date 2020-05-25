14d93b67835b88fbb05b71b689c8755e45b2070d
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-05-24 19:31:05 -0500

    Fix MenuSider key error in client

--- client/src/components/Admin/MenuSider/MenuSider.js
+++ client/src/components/Admin/MenuSider/MenuSider.js
@@ -1,23 +1,24 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import {Layout,Menu} from "antd"
 import{HomeOutlined, UserOutlined} from "@ant-design/icons"
 
 import "./MenuSider.scss";
 
-export default function MenuSider(props){
+ function MenuSider(props){
     const {Sider} = Layout;
-    const {menuCollapsed} = props;
+    const {menuCollapsed, location} = props;
+
     return (
         <Sider className="admin-sider" collapsed={menuCollapsed}>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-                <Menu.Item key="1">
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={[location.pathname]}>
+                <Menu.Item key="/admin">
                     <Link to={"/admin"}>
                     <HomeOutlined />
                     <span className="nav-text">Home</span>
                     </Link>
                 </Menu.Item>
-                <Menu.Item key="2">
+                <Menu.Item key="/admin/users">
                     <Link to={"/admin/users"}>
                     <UserOutlined />
                     <span className="nav-text">Usuarios</span>
@@ -27,3 +28,5 @@ export default function MenuSider(props){
         </Sider>
     );
 }
+
+export default withRouter(MenuSider);
