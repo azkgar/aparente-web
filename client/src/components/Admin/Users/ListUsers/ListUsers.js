87f0f098c6539f08f7006b4e7e283afe746c7315
Azkary Garcia <macbookpro@Azkarys-MacBook-Pro.local>
2020-05-25 19:27:23 -0500

    Creates new buttor for create user at client

--- client/src/components/Admin/Users/ListUsers/ListUsers.js
+++ client/src/components/Admin/Users/ListUsers/ListUsers.js
@@ -18,17 +18,30 @@ export default function ListUsers(props) {
     const [modalTitle, setModalTitle] = useState("");
     const [modalContent, setModalContent] = useState(null);
 
+    const addUserModal = () => {
+        setIsVisibleModal(true);
+        setModalTitle("Crear nuevo usuario");
+        setModalContent(<div> <h1>Formulario</h1></div>);
+    }
+
     return(
         <div className = "list-users">
-            <div className = "list-users__switch">
-                <Switch 
-                    defaultChecked
-                    onChange = {() => setViewUsersActives(!viewUsersActives)}
-                />
-                <span>
-                    {viewUsersActives ? "Usuarios Activos" : "Usuarios Inactivos"}
-                </span>
+
+            <div className = "list-users__header">
+                <div className = "list-users__header-switch">
+                    <Switch 
+                        defaultChecked
+                        onChange = {() => setViewUsersActives(!viewUsersActives)}
+                    />
+                    <span>
+                        {viewUsersActives ? "Usuarios Activos" : "Usuarios Inactivos"}
+                    </span>
+                </div>
+                <Button type = "primary" onClick = {addUserModal}>
+                    Nuevo Usuario
+                </Button>
             </div>
+            
             {viewUsersActives ? <UsersActive usersActive = {usersActive} setIsVisibleModal = {setIsVisibleModal} setModalTitle = {setModalTitle} setModalContent = {setModalContent} setReloadUsers = {setReloadUsers} /> : <UsersInactive usersInactive = {usersInactive} setReloadUsers = {setReloadUsers} />}
             <Modal title = {modalTitle} isVisible={isVisibleModal} setIsVisible  = {setIsVisibleModal}>
                 {modalContent}
