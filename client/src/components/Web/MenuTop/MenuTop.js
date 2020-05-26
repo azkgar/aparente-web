a298cdacd0e89e0032e9e30af14dabd2a2349433
Azkary Garcia <macbookpro@Azkarys-MacBook-Pro.local>
2020-05-26 15:16:11 -0500

    Sets color background to light black and adds logo image to navbar

--- client/src/components/Web/MenuTop/MenuTop.js
+++ client/src/components/Web/MenuTop/MenuTop.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import {Menu} from "antd";
 import {Link} from "react-router-dom";
+import logo from "../../../assets/img/png/APARENTE_logo_blanco_sinfondo.png"
 
 import "./MenuTop.scss";
 
@@ -8,7 +9,9 @@ export default function MenuTop(props) {
     return(
         <Menu className = "menu-top" mode = "horizontal">
             <Menu.Item className = "menu-top__logo">
-                Logo
+                <Link to = {"/"}>
+                    <img src = {logo} alt = "Aparente" />
+                </Link>
             </Menu.Item>
             <Menu.Item className = "menu-top__item">
                 <Link to = {"/contact"}>Contacto</Link>
