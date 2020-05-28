5f844e33190ca2eafaefca125c08225a6f398f74
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-05-27 21:12:34 -0500

    Add basic layout for post page at admin console

--- client/src/pages/Admin/Blog/Blog.js
+++ client/src/pages/Admin/Blog/Blog.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Button, notification} from "antd";
+import Modal from "../../../components/Modal";
 
 import "./Blog.scss";
 
 export default function Blog() {
+    const [modalTitle, setModalTitle] = useState("");
+    const [modalContent, setModalContent] = useState(null);
+    const [isVisibleModal, setIsVisibleModal] = useState(false);
+    
+
     return (
-        <div>
-            <h1>Blog</h1>
+        <div className = "blog">
+            <div className = "blog__add-post">
+                <Button type = "primary">
+                    Nuevo post
+                </Button>
+            </div>
+            <h1>Post List</h1>
+            <h2>Paginación</h2>
+
+            <Modal
+                title = {modalTitle}
+                isVisible = {isVisibleModal}
+                setIsVisible = {setIsVisibleModal}
+                width = "75%"
+            />
         </div>
     )
 }
