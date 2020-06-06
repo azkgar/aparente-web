43370455bd660f399fd99d0cc4adec994c6c9a3f
Azkary Garcia <macbookpro@Azkarys-MacBook-Pro.local>
2020-06-06 00:38:43 -0500

    Fix post banner and create files for category display

--- client/src/components/Web/Blog/Categories/Categories.js
+++ client/src/components/Web/Blog/Categories/Categories.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import CategoriesBanner from "./CategoriesBanner";
+import MainBanner from "../MainBanner";
+import CategoryPage from "./CategoryPage";
 
 import "./Categories.scss";
 
 export default function Categories() {
     return (
         <div>
+            <MainBanner />
             <CategoriesBanner />
         </div>
     )
---
+++ client/src/components/Web/Blog/Categories/CategoryPage/CategoryPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./CategoryPage.scss";
+
+export default function CategoryPage() {
+    return (
+        <div>
+            <h1>Página de URL</h1>
+        </div>
+    )
+}
