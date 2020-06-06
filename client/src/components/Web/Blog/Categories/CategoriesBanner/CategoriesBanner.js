8a4ede4c21492f9cdcc12863a6062600b8002dee
Azkary Garcia <macbookpro@Azkarys-MacBook-Pro.local>
2020-06-06 00:17:26 -0500

    Complete categories list with responsive design

--- client/src/components/Web/Blog/Categories/CategoriesBanner/CategoriesBanner.js
+++ client/src/components/Web/Blog/Categories/CategoriesBanner/CategoriesBanner.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {getCategoriesApi} from "../../../../../api/category";
 import {Link} from "react-router-dom";
+import {Spin} from "antd";
 
 
 import "./CategoriesBanner.scss";
@@ -18,15 +19,19 @@ export default function CategoriesBanner() {
         });
     }, []);
 
-    console.log(categories);
+    if(!categories){
+        return (
+            <Spin tip = "Cargando" style = {{width: "100%", padding: "200px 0"}} />
+        )
+    }
     
 
     return (
-        <div>
-            <h2>Encuentra publicaciones relacionadas al tema de tu interés</h2>
+        <div className = "categories-list">
+            <h2>¿Buscas un tema específico?</h2>
             {categories.map(category => (
                 <Link to ={`categorias/${category.tag.toLowerCase()}`} key = {category._id}>
-                <div>
+                <div className = "categories-list__item">
                     <img alt = {category.tag} src = {category.avatar ? require(`../../../../../../../server/uploads/categories/${category.avatar}`) : require("../../../../../assets/img/png/Missing.png") }/>
                     <h4>{category.tag}</h4>
                 </div>
