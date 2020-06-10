cc0c1f8c673800e3253a7b22f3b8e80e3f3c1125
Azkary Garcia <macbookpro@Azkarys-MacBook-Pro.local>
2020-06-10 14:12:18 -0500

    Refresh time at real time for whatsapp contact

--- client/src/components/Web/Contact/WhatsApp/WhatsApp.js
+++ client/src/components/Web/Contact/WhatsApp/WhatsApp.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Row, Col, Input, Button, Form} from "antd";
+import mobile from "../../../../assets/img/png/APARENTE-WhatsApp.png";
+import moment from "moment";
+
+import "./WhatsApp.scss";
 
 export default function WhatsApp() {
+    const [time,setTime] = useState(moment().format("LT"));
+
+    useEffect(() => {
+        const interval = setInterval(() => setTime(moment().format("LT")), 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
     return (
-        <div>
-            WhatsApp
+        <div className = "whatsapp-form">
+            <Row>
+                <Col md = {12} sm = {24} className = "whatsapp-form__description">
+                    <p>Contáctanos</p>
+                </Col>
+                <Col md = {12} sm = {24} className = "whatsapp-form__form">
+                    <img alt = "Manda un WhatsApp al " src = {mobile} />
+                    <p>{time}</p>
+                    <Form>
+                        <Form.Item>
+                            <Input autoFocus = "autofocus"></Input>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button></Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
         </div>
-    )
+    );
 }
