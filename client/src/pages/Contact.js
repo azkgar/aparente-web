9705627a3c0679b4e3b91f41647f23c50ad27d20
Azkary Garcia <macbookpro@Azkarys-MacBook-Pro.local>
2020-06-10 12:30:15 -0500

    Create whatsapp contact files

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MainBanner from "../components/Web/Contact/MainBanner";
+import WhatsApp from "../components/Web/Contact/WhatsApp";
 import {Helmet} from "react-helmet";
 
 export default function Contact() {
@@ -9,9 +10,8 @@ export default function Contact() {
             <title>Aparente | Contacto</title>
             <meta name = "description" content = "Página donde puedes encontrar todos nuestros puntos de contacto."/>
         </Helmet>
-        <div>
-            <MainBanner />
-        </div>
+        <MainBanner />
+        <WhatsApp />
         </>
-    )
+    );
 }
