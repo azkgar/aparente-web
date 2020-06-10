4190cc08487c0d16278a65514f2dd26b15e1889b
Azkary Garcia <macbookpro@Azkarys-MacBook-Pro.local>
2020-06-10 15:15:12 -0500

    Send message to whatsapp

--- client/src/components/Web/Contact/WhatsApp/WhatsApp.js
+++ client/src/components/Web/Contact/WhatsApp/WhatsApp.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Row, Col, Input, Button, Form} from "antd";
+import {SendOutlined} from "@ant-design/icons";
 import mobile from "../../../../assets/img/png/APARENTE-WhatsApp.png";
 import moment from "moment";
 
@@ -7,6 +8,7 @@ import "./WhatsApp.scss";
 
 export default function WhatsApp() {
     const [time,setTime] = useState(moment().format("LT"));
+    const [message, setMessage] = useState("");
 
     useEffect(() => {
         const interval = setInterval(() => setTime(moment().format("LT")), 1000);
@@ -15,6 +17,12 @@ export default function WhatsApp() {
         };
     }, []);
 
+    function handleChange(e){
+        e.preventDefault();
+        window.open(`https://api.whatsapp.com/send?phone=525612982728&text=${message}`, "__blank")
+        setMessage("");
+    }
+    
     return (
         <div className = "whatsapp-form">
             <Row>
@@ -24,12 +32,12 @@ export default function WhatsApp() {
                 <Col md = {12} sm = {24} className = "whatsapp-form__form">
                     <img alt = "Manda un WhatsApp al " src = {mobile} />
                     <p>{time}</p>
-                    <Form>
+                    <Form onSubmit = {handleChange}>
                         <Form.Item>
-                            <Input autoFocus = "autofocus"></Input>
+                            <Input autoFocus = "autofocus" value = {message} onChange = {e => setMessage(e.target.value)}></Input>
                         </Form.Item>
                         <Form.Item>
-                            <Button></Button>
+                            <Button type = "primary" shape = "circle" size = "small" icon = {<SendOutlined style = {{ color: "white" }}/> } htmlType = "submit" onClick = {handleChange}></Button>
                         </Form.Item>
                     </Form>
                 </Col>
