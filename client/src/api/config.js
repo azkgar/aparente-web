d10936a120b77dfda5b6ea148d2a6b0e018d93ac
Azkary Garcia <macbookpro@Azkarys-MacBook-Pro.local>
2020-06-20 19:09:12 -0500

    Add heroku link

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,2 +1,3 @@
-export const basePath = "http://localhost:5000/api";
+//export const basePath = "http://localhost:5000/api";
+export const basePath = "http://aparente-server.herokuapp.com/api"
 export const apiVersion = "v1";
