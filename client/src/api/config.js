064f09619056d86fc717001da8834aa38a4cddac
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-06-20 20:42:49 -0500

    Build final app

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,3 +1,2 @@
-//export const basePath = "http://localhost:5000/api";
-export const basePath = "http://aparente-server.herokuapp.com/api"
+export const basePath = "https://aparente-server.herokuapp.com/api"
 export const apiVersion = "v1";
